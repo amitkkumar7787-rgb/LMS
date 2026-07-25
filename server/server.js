f6aa3b596cfa5e41dc1845fb293fe172c3e74f41
Tushar kumar <narutoboi41@gmail.com>
2026-07-26 00:00:20 +0530

    dekkoo

--- server/server.js
+++ server/server.js
@@ -2,29 +2,23 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import ConnectDb from "./config/mongodb.js";
-import { clerkwebhooks } from "./controllers/webhooks.js"; // <-- small w
+import { clerkWebhooks } from "./controllers/webhooks.js";
 
 dotenv.config();
 
 const app = express();
 
-// Database Connection
 await ConnectDb();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.get("/", (req, res) => {
   res.send("API Working");
 });
 
 app.post("/clerk", clerkWebhooks);
 
-// Port
-const PORT = process.env.PORT || 5005;
+// ❌ app.listen(...) mat karo
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default app;
