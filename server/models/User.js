5880fd3e2762d118df684f17b46cfd7827eb871a
Tushar kumar <narutoboi41@gmail.com>
2026-07-26 00:17:52 +0530

    oh

--- server/models/User.js
+++ server/models/User.js
@@ -10,7 +10,7 @@ let userSchema = new mongoose.Schema(
             {
                 type : mongoose.Schema.Types.ObjectId,
                 ref : 'Course'
-            }
+            } 
         ]
     }
 )
